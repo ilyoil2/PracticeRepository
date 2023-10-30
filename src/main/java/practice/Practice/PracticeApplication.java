4e85b75409e1403d70fb1c56645e880c5308de1d
tae.y <0211ilyoil@gmail.com>
2023-10-30 09:00:29 +0900

    비동기 처리 연습

--- src/main/java/practice/Practice/PracticeApplication.java
+++ src/main/java/practice/Practice/PracticeApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableAsync;
 import practice.Practice.global.security.jwt.JwtProperties;
 
+@EnableAsync
 @SpringBootApplication
 @ConfigurationPropertiesScan
 @EnableConfigurationProperties(JwtProperties.class)
@@ -14,5 +16,4 @@ public class PracticeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PracticeApplication.class, args);
 	}
-
 }
